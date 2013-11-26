58f18ec33077195fb7b48c823df9fe918a74f783
Marvin Barretto <marvinbarretto@gmail.com>
2013-11-26 21:31:57 +0000

    add maps

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,3 @@
+Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/2176f73e52484ee99956be6c04a57c9d/997/256/{z}/{x}/{y}.png"
+Leaflet.attribution = "Your attribution statement"
+Leaflet.max_zoom = 18
