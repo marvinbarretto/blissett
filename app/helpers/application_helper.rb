ee5621927535514c2dfcd92d791de83a2993233f
Marvin Barretto <marvinbarretto@gmail.com>
2013-11-26 21:45:12 +0000

    Calc distance from watford

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def distance a, b
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 0.621371192      # Radius in miles
+
+    dlon_rad = (b[1]-a[1]) * rad_per_deg  # Delta, converted to rad
+    dlat_rad = (b[0]-a[0]) * rad_per_deg
+
+    lat1_rad, lon1_rad = a.map! {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = b.map! {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math.asin(Math.sqrt(a))
+
+    delta = rm * c # Delta in miles
+
+    delta.round
+  end
+
+  def watford_coords
+    [51.65648900083038, -0.3903200011700392]
+  end
+  
 end
--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,25 +1,3 @@
 module PlacesHelper
 
-  def distance a, b
-    rad_per_deg = Math::PI/180  # PI / 180
-    rkm = 6371                  # Earth radius in kilometers
-    rm = rkm * 1000             # Radius in meters
-
-    dlon_rad = (b[1]-a[1]) * rad_per_deg  # Delta, converted to rad
-    dlat_rad = (b[0]-a[0]) * rad_per_deg
-
-    lat1_rad, lon1_rad = a.map! {|i| i * rad_per_deg }
-    lat2_rad, lon2_rad = b.map! {|i| i * rad_per_deg }
-
-    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
-    c = 2 * Math.asin(Math.sqrt(a))
-
-    rm * c # Delta in meters
-  end
-
-  def watford_latlng
-    [17.99273099915159, -76.7920088977553]
-  end
-
-
 end
