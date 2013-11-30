277bf5329546795c4806fddc799f81a53d4683d9
Marvin Barretto <marvinbarretto@gmail.com>
2013-11-30 03:30:21 +0000

    Refactor people index

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,7 +3,7 @@ class Person < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name,  presence: true
 
-  has_one :place
+  belongs_to :place
   has_one :country, through: :place
   
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,7 @@ class Place < ActiveRecord::Base
   validates :name, presence: true
   
   has_many :clubs
+  has_many :people
   
   belongs_to :country
 
